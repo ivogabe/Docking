83ada9d5725b43f67f4e61a68864286199a3edd7
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2016-04-29 13:03:01 +0200

    Code style

--- src/Molecule.cs
+++ src/Molecule.cs
@@ -12,29 +12,28 @@ namespace Docking {
 		public Molecule(string fileName) {
 			// TODO: Load data from PDB file
 			StreamReader read = new StreamReader(fileName);
-			List<string> file = new List<string>();
+			List<string> atoms = new List<string>();
 			while (read.Peek() >= 0) {
 				string line = read.ReadLine();
 				if (line.StartsWith("ATOM")) {
-					file.Add(line);
+					atoms.Add(line);
 				}				
 			}
-			Size = file.Count;
+			Size = atoms.Count;
 			X = new float[Size];
 			Y = new float[Size];
 			Z = new float[Size];
 			Diameter = new float[Size];
 			Charge = new float[Size];
 			int t = 0;
-			foreach(string r in file) {
+			foreach(string r in atoms) {
 				X[t] = float.Parse(r.Substring(30, 8));
 				Y[t] = float.Parse(r.Substring(38, 8));
 				Z[t] = float.Parse(r.Substring(46, 8));
 				Charge[t] = float.Parse(r.Substring(55, 8));
 				Diameter[t] = float.Parse(r.Substring(62, 6)) * 2;
-				t ++;
+				t++;
 			}
-			 
 		}
 		
 		public Vector GetAtom(int id) {
