20a8746a5af267e58929820dd157fd403b92c798
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2016-04-29 11:05:52 +0200

    Implement Program.cs

--- Program.cs
+++ Program.cs
@@ -3,8 +3,18 @@ using System;
 namespace Docking {
 	public class Program {
 		public static void Main(string[] args) {
-			Console.WriteLine("Hello World");
-			Console.Read();
+			if (args.Length != 2) {
+				Console.WriteLine("Docking");
+				Console.WriteLine("Usage:");
+				Console.WriteLine("  dnx run protein.pdb ligand.pdb output.pdb");
+			}
+			Molecule moleculeA = new Molecule(args[0]);
+			Molecule moleculeB = new Molecule(args[1]);
+			Grid grid = new Grid(moleculeA, moleculeB);
+			Search search = new Search(grid);
+			search.Run(10000);
+			Console.WriteLine("Best score " + search.Best.Value);
+			Output.Write(args[2], moleculeA, moleculeB, search.Best.Transform);
 		}
 	}
 }
---
+++ src/Output.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Docking {
+	class Output {
+		public static void Write(string fileName, Molecule moleculeA, Molecule moleculeB, Transformation transform) {
+			// TODO
+		}
+	}
+}
