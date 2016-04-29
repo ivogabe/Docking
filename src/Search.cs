2abc8035c2717db709d3c139be4eeecbee77e394
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2016-04-29 12:53:42 +0200

    Implement quadratic convergence

--- src/Search.cs
+++ src/Search.cs
@@ -74,12 +74,27 @@ namespace Docking {
 			float dPitch = randomFloat(sizeParameter / 2);
 			float dRoll = randomFloat(sizeParameter / 2);
 			
+			// Choose 3 points (0, 1, x3) and plot a parabola between them.
+			// Then search the minimum of the parabola, if it exists.
+			
 			Transformation a = Current.Transform.Modify(dX, dY, dZ, dYaw, dPitch, dRoll);
-			// int factor = random.Next(1) == 1 ? 2 : -1;
-			// Transformation b = Current.Transform.Modify(factor * dX, factor * dY, factor * dZ, factor * dYaw, factor * dPitch, factor * dRoll);
+			float x3 = random.Next(1) == 1 ? 2 : -1;
+			Transformation b = Current.Transform.Modify(x3 * dX, x3 * dY, x3 * dZ, x3 * dYaw, x3 * dPitch, x3 * dRoll);
 			
 			float aValue = grid.GetValue(a);
-			// float bValue = grid.GetValue(b);
+			float bValue = grid.GetValue(b);
+			
+			float x4 = findExtreme(0, 1, x3, Current.Value, aValue, bValue);
+			Transformation c = Current.Transform.Modify(x4 * dX, x4 * dY, x4 * dZ, x4 * dYaw, x4 * dPitch, x4 * dRoll);
+			float cValue = grid.GetValue(c);
+			
+			if (aValue <= bValue && aValue <= cValue) {
+				return new State(a, aValue);
+			} else if (bValue <= cValue) {
+				return new State(b, bValue);
+			} else {
+				return new State(c, cValue);
+			}
 			
 			// TODO: Plot graph and guess best position
 			return new State(a, aValue);
@@ -87,6 +102,16 @@ namespace Docking {
 		
 		private float randomFloat(float size) {
 			return size * (float) random.NextDouble();
+		}
+		
+		/**
+		 * Returns the extreme of a parabola
+		 */
+		private float findExtreme(float x1, float x2, float x3, float y1, float y2, float y3) {
+			float a1 = x1 * (y2 - y3);
+			float a2 = x2 * (y3 - y1);
+			float a3 = x3 * (y1 - y2);
+			return (x1 * a1 + x2 * a2 + x3 * a3) / (a1 + a2 + a3);
 		}
 	}
 }
